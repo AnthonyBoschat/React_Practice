b0591dd1c055627a7d40838c9f453752d612b17f
AnthonyBoschat <anthonyboschat.dev@hotmail.com>
2023-10-07 16:30:23 +0200

    Apprentissage base react

--- test_react/src/App.js
+++ test_react/src/App.js
@@ -13,15 +13,48 @@ class App extends React.Component
       {id: 2, nom: "Maurane Lorjou"},
       {id: 3, nom: "Mathias Charpentier"}
     ],
-    compteur: 0
+    nouveau_client : ""
+
   }
 
   // Comportement
-  handleClick = () =>
+  // Pour supprimer un client
+  handleDelete = (id) =>
   {
+    // On créé une copie du tableau du state
     const clients = this.state.clients.slice()
-    clients.push({id: 4, nom: "Anne Dupont"})
+
+    // Trouve la place du client dans le tableau grâce à son ID
+    const index = clients.findIndex((client => client.id === id ))
+
+    // On supprime ce client
+    clients.splice(index, 1)
+
+    // On met à jour le state
     this.setState({clients: clients})
+  }
+
+  // Soumission du formulaire pour ajouter un client
+  handleSubmit = (event) => 
+  {
+    // On empêche le rechargement de la page
+    event.preventDefault()
+    const id = new Date().getTime()
+    const nom = this.state.nouveau_client
+
+    const client = {id: id, nom: nom}
+    const clients = this.state.clients.slice()
+    clients.push(client)
+    this.setState({clients: clients, nouveau_client: ""})
+  }
+
+  // Quand l'input change
+  handleChange = (event) => 
+  {
+    // On récupère la valeur de l'input
+    const value = event.target.value
+    // On change la valeur du state du novueau_client
+    this.setState({nouveau_client: value})
   }
 
 
@@ -34,12 +67,11 @@ class App extends React.Component
     return(
       <div>
         <h1>{this.state.liste_name}</h1>
-        <button onClick={this.handleClick}>controle</button>
         <ul>
-          {this.state.clients.map(client => (<li>{client.nom} <button>Supprimer</button></li>))}
+          {this.state.clients.map(client => (<li>{client.nom} <button onClick={() => this.handleDelete(client.id)}>Supprimer</button></li>))}
         </ul>
-        <form>
-          <input type='text' placeholder='Ajouter un client'/>
+        <form onSubmit={this.handleSubmit}>
+          <input value={this.state.nouveau_client} onChange={this.handleChange} type='text' placeholder='Ajouter un client'/>
           <button>Ajouter</button>
         </form>
       </div>
