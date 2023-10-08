6e1e36b922cfdd31d81e3b308d3a88f9d2373b4e
AnthonyBoschat <anthonyboschat.dev@hotmail.com>
2023-10-08 22:20:11 +0200

    Creation du fichier index.js

--- test_react/src/App.jsx
+++ test_react/src/App.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
-import './App.css';
 import Client from "./Client";
 import Formulaire from "./Formulaire";
-import Counter from "./Counter";
 
 const App = () =>
 {
