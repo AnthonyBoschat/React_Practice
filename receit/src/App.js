1bc8bffd74c23346415e51b47e60950939219f5f
AnthonyBoschat <anthonyboschat.dev@hotmail.com>
2023-10-13 12:16:41 +0200

    Nouveau projet, recette

---
+++ receit/src/App.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./css/App.css"
+
+function App() {
+
+  
+
+  return (
+    <main>
+      <section>Cookie</section>
+      <section>Chantilly</section>
+      <section>Crême anglaise</section>
+    </main>
+  );
+}
+
+export default App;
