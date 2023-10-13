fe4b1bbae0f0931a48a7177e79e64b5e9889ecbd
AnthonyBoschat <anthonyboschat.dev@hotmail.com>
2023-10-13 15:43:01 +0200

    Requête à l'API

--- receit/src/App.js
+++ receit/src/App.js
@@ -1,21 +1,28 @@
 import { useState } from "react";
 import "./css/App.css"
 import Button from "./Button"
+import ElementContext from "./ElementContext";
 
 function App() {
 
-  const [listOfElement, update_listOfElement] = useState([
-    {id: 1, name: "Cookie"},
-    {id: 2, name: "Chantilly"},
-    {id: 3, name: "Crême anglaise"},
+  const [listOfElement, updateListOfElement] = useState([
+    {id: 1, name: "Cookie", engname: "Cookie"},
+    {id: 2, name: "Tarte aux pommes", engname: "Apple pie"},
+    {id: 3, name: "Yaourt", engname: "Yogurt"},
   ])
 
-  
+  const valueElementContext = {
+    listOfElement,
+    updateListOfElement
+  }
 
   return (
-    <main>
-      {listOfElement.map((element) => <Button key={element.id} element={element}/> )}
-    </main>
+    <ElementContext.Provider value={valueElementContext}>
+      <main>
+        {listOfElement.map((element) => <Button key={element.id} element={element}/> )}
+        <div id="destination"></div>
+      </main>
+    </ElementContext.Provider>  
   );
 }
 
--- receit/src/Button.jsx
+++ receit/src/Button.jsx
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useContext } from "react";
+import ElementContext from "./ElementContext";
 
 function Button({element}) {
 
-    function handleClick(){
-        console.log("controle")
+    const {listOfElement, updateListOfElement} = useContext(ElementContext)
+
+    function handleClick(event){
+        // On récupère le nom du produit cliqué
+        const eventName = event.target.textContent
+        // On récupère l'index de l'élément correspondant à ce nom
+        const index = listOfElement.findIndex((element) => element.name === eventName)
+        // On récupère son nom en Anglais
+        const name = listOfElement[index].engname
+        // On effectue une requêt eà l'API en se sservant de son nom
+        // On définie notre clef API, et l'adresse
+        const APIkey = "f26bb86aabe743069b136fed5a400204"
+        const adress = "https://api.spoonacular.com/recipes/complexSearch"
+        // On effectue la requête en passant le name comme propriété query
+        fetch(`${adress}?apiKey=${APIkey}&query=${name}`)
+        .then((response) => response.json())
+        .then(response => {
+            console.log(response)
+            const image = response.results[0].image
+            console.log(image)
+            const destination = document.getElementById("destination")
+            let new_image = document.createElement("img")
+            new_image.src = image
+            destination.appendChild(new_image) 
+        })
+
+
+
+        //const keyAPI = "f26bb86aabe743069b136fed5a400204"
+        //fetch("https://api.spoonacular.com/recipes/complexSearch")
     }
 
     return(
-        <button onClick={handleClick}> {element.name} </button>
+        <button onClick={handleClick} id={element.id}>{element.name}</button>
     );
 
 }
