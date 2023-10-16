40e04f875ad9ed7c4f7745237d9ebae2693db9b0
AnthonyBoschat <anthonyboschat.dev@hotmail.com>
2023-10-16 22:17:49 +0200

    Mise en place de la barre d'autocomplete

--- receit/src/App.js
+++ receit/src/App.js
@@ -1,34 +1,14 @@
 import { useState } from "react";
 import "./css/App.css";
-import Button from "./Button";
-import ElementContext from "./ElementContext";
 import SearchBar from "./SearchBar";
-import "./css/ReceitStorage.css";
 import { injectAnimation } from "./global_tools";
-import ReceitStorage from "./ReceitStorage";
 
 function App() {
 
-  const [listOfElement, updateListOfElement] = useState([
-    {id: 1, name: "Cookie", hidden: false},
-    {id: 2, name: "Apple pie", hidden: false},
-    {id: 3, name: "Yogurt", hidden: false},
-  ])
-
-  const valueElementContext = {
-    listOfElement,
-    updateListOfElement
-  }
-
-
-
   return (
-    <ElementContext.Provider value={valueElementContext}>
       <main>
-        <ReceitStorage />
-        <div id="searchBar"><SearchBar /></div>
+        <SearchBar />
       </main>
-    </ElementContext.Provider>  
   );
 }
 
--- receit/src/SearchBar.jsx
+++ receit/src/SearchBar.jsx
@@ -3,48 +3,53 @@ import "./css/SearchBar.css"
 import ElementContext from "./ElementContext";
 
 function SearchBar(){
+    
 
     const [searchBarValue, updatesearchBarValue] = useState("")
+    const [suggestions, updateSuggestions] = useState ([])
     const {listOfElement, updateListOfElement} = useContext(ElementContext)
     const API_KEY = "f26bb86aabe743069b136fed5a400204"
 
     // Lorsque la valeur de l'input search change
     async function handleChange(event){
         // On update la value
-        updatesearchBarValue(event.target.value)
-        // On utilise l'API pour récupérer l'aide de la saisie semi-automatique
-        const url = new URL(`https://api.spoonacular.com/recipes/autocomplete`)
-        url.searchParams.append("apiKey", API_KEY)
-        url.searchParams.append("number", "10")
-        url.searchParams.append("query", `${event.target.value}`)
-        const response = await fetch(url)
-        const data = await response.json()
-        console.log(data)
+        let value = event.target.value
+        updatesearchBarValue(value)
+        if(value.length > 2){
+            // On utilise l'API pour récupérer l'aide de la saisie semi-automatique
+            const url = new URL(`https://api.spoonacular.com/recipes/autocomplete`)
+            url.searchParams.append("apiKey", API_KEY)
+            url.searchParams.append("number", "10")
+            url.searchParams.append("query", `${event.target.value}`)
+            const response = await fetch(url)
+            let data = await response.json()
+            if(data.length != 0){
+                updateSuggestions(data)
+            }else{
+                data = [{
+                    index:0,
+                    title:"( Aucune recette )"
+                }]
+                updateSuggestions(data)
+            }
+            
+        }else{
+            updateSuggestions([])
+        }
     }
     
     return(
-        <input value={searchBarValue} type="search" onChange={handleChange}></input>
+        <div id="searchBar_englobe">
+            <div id="searchBar">
+                <input list="suggestions" value={searchBarValue} type="search" onChange={handleChange}></input>
+            </div>
+            {suggestions.length > 0 && (
+                    <div id="autocompleteBox">
+                        {suggestions.map((suggestion, index) => (<button key={index}>{suggestion.title}</button>))}
+                    </div>
+                )}
+        </div>
     )
 }
 
 export default SearchBar;
-
-/* Dans handleChange
-
-// On update la value
-        updatesearchBarValue(event.target.value)
-        // On créé une copie de la liste des éléments
-        const copy_listOfElement = [...listOfElement]
-        // On cherche les éléments dans les boutons qui correspondent, on efface les autres
-        copy_listOfElement.forEach(element => {
-            if(!element.name.toLowerCase().includes(event.target.value.toLowerCase()))
-            {
-                element.hidden = true
-            }
-            else{
-                element.hidden = false
-            }
-        updateListOfElement(copy_listOfElement)
-        })
-
-*/
