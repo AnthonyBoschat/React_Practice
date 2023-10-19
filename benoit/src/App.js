9069c89a586156817045fe312f0f06fe6bb0a010
AnthonyBoschat <anthonyboschat.dev@hotmail.com>
2023-10-20 01:16:25 +0200

    J'sais plus

---
+++ benoit/src/App.js
@@ -0,0 +1,15 @@
+import "./Style/App.css"
+
+function App() {
+  return (
+    <main>
+      <div id="form">
+        <input type="text"></input>
+        <button>Start</button>
+      </div>
+      <div id="destination"></div>
+    </main>
+  );
+}
+
+export default App;
