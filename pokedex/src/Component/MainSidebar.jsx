e682a211a4980f2abad165f9eff2163512d78feb
AnthonyBoschat <anthonyboschat.dev@hotmail.com>
2023-11-07 17:44:18 +0100

    Mise a jour majeur de la requête API

--- pokedex/src/Component/MainSidebar.jsx
+++ pokedex/src/Component/MainSidebar.jsx
@@ -3,19 +3,12 @@ import { StateContext } from "../Context/StateContext"
 function MainSidebar(){
 
     // State et Context
-    const {pokemonsList, setPokemonsList, logoVisible, setLogoVisible, boutonsSelectType, setBoutonsSelectType, filtrage} = useContext(StateContext)
-    const [boutons, setBoutons] = useState([
-        {id:1, name:"Première génération", generation:1, selected:false},
-        {id:2, name:"Deuxième génération", generation:2, selected:false},
-        {id:3, name:"Troisième génération", generation:3, selected:false},
-        {id:4, name:"Quatrième génération", generation:4, selected:false},
-    ])
+    const {pokemonsList, setPokemonsList, logoVisible, setLogoVisible, boutonsSelectType, setBoutonsSelectType, filtrage, boutons, setBoutons} = useContext(StateContext)
 
     // Methode
-    const apiRequestPokemonGeneration = (event) => {
+    const showPokemonOfGeneration = (event) => {
         // On ajoute ou retire la classe generationSeleted
         event.target.classList.contains("generationSelected") ? event.target.classList.remove("generationSelected") :event.target.classList.add("generationSelected")
-
         // On créé une copie du tableau des boutons de generation
         const copyBoutons = [...boutons]
         // On change la clef selected en fonction
@@ -26,38 +19,25 @@ function MainSidebar(){
         })
         // On setState les boutons
         setBoutons(copyBoutons)
-
-        // On récupère les boutons selectionner du nouveau state
-        const boutonClicked = copyBoutons.filter((bouton) => bouton.selected === true)
-        // S'il y a une génération de sélectionner au moin
-        if(boutonClicked.length > 0){
-            // retourAllFetchAPI est une promesse qui contient les multiples fetch
-            const retourAllFetchAPI = boutonClicked.map(bouton =>
-                fetch(`https://pokebuildapi.fr/api/v1/pokemon/generation/${bouton.generation}`)
-                    .then(response => {
-                        return response.json();
-                    })
-            );
-
-            // Promise.all : Quand toutes les promesses de retourAllFetchAPI ont été résolu
-            Promise.all(retourAllFetchAPI)
-            .then(pokemons => {
-                // pokemons est un tableau de tableau, récupérer par les multiples fetch, on concatène tout ça dans un seul tableau : allPokemons
-                const allPokemons = [].concat(...pokemons);
-                // On passe cet unique tableau dans le filtrage
-                filtrage(allPokemons);
-                setLogoVisible(false);
-            })
-        } else {
-            setLogoVisible(true);
-        }
-        
+        filtrage(pokemonsList)
     }
 
-
+    // Fonction qui récupère toute la liste des pokemons au chargement de la page
+    useEffect(() => {
+        fetch(`https://pokebuildapi.fr/api/v1/pokemon`)
+        .then(response => response.json())
+        .then(pokemons => {
+            pokemons.forEach(pokemon => {
+                pokemon.visible = false
+                pokemon.generationSelected = false
+            })
+            setPokemonsList(pokemons)
+        })
+    }, [])
+    
     const generateBoutons = (bouton) => {
         return(
-            <button key={bouton.id} onClick={apiRequestPokemonGeneration}>{bouton.name}</button>
+            <button key={bouton.id} onClick={showPokemonOfGeneration}>{bouton.name}</button>
         )
     }
 
--- pokedex/src/Context/StateContext.jsx
+++ pokedex/src/Context/StateContext.jsx
@@ -14,6 +14,12 @@ export const StateProvider = ({children}) => {
     })
     const [pokemonsList, setPokemonsList] = useState([])
     const [logoVisible, setLogoVisible] = useState(true)
+    const [boutons, setBoutons] = useState([
+        {id:1, name:"Première génération", generation:1, selected:false},
+        {id:2, name:"Deuxième génération", generation:2, selected:false},
+        {id:3, name:"Troisième génération", generation:3, selected:false},
+        {id:4, name:"Quatrième génération", generation:4, selected:false},
+    ])
     const [boutonsSelectType, setBoutonsSelectType] = useState([
         {name:"Feu", selected:false},
         {name:"Plante", selected:false},
@@ -36,68 +42,123 @@ export const StateProvider = ({children}) => {
     ]);
     // fonction qui permet de filtrer les pokemons selon les types selectionner et de setState la nouvelle liste
     const filtrage = (pokemonsList) => {
+
         // On créé une copie de la liste de tout les pokemons
         const copyPokemonsList = [...pokemonsList]
-        // On créé un tableau qui correspond à tout les types selectionner
-        const tableauTypeSelected = boutonsSelectType.filter((element) => element.selected === true)
-        // On prépare une string de filtre qui va servir à écrire tout les npm de type séléctionner
-        let typeSelected = ""
-        // On rempli le string de filtre
-        tableauTypeSelected.forEach((type) => typeSelected += type.name)
-        // Si le nombre de type selectionner est supérieur à 0
-        if(tableauTypeSelected.length != 0){
-            // Pour chaque objet pokemon de la liste
-            copyPokemonsList.forEach(pokemon => {
-                // On prépare un tableau qui va contenir les types du pokemon en cours d'instance
-                const tableauThisPokemonType = []
-                // Si le pokemon en cours d'instance a moin de type que le nombre de filtre en cours, il est rendu invisible, il ne rentre pas dans les critères de selection
-                if(pokemon.apiTypes.length < tableauTypeSelected.length){pokemon.visible = false}
-                // Si le pokemon en cours d'instance a autant ou plus de type que le nombre de filtre en cours
-                else{
-                    // Pour chaque types de ce pokemon
-                    pokemon.apiTypes.forEach(thisPokemonType => {
-                        // On rempli le tableau
-                        tableauThisPokemonType.push(thisPokemonType.name)
-                    })
-                    // S'il n'y a qu'un seul filtre
-                    if(tableauTypeSelected.length === 1){
-                        // pour ce pokemon,on verifie si au moin l'un de ces types est includes dans le string de filtre, si oui, renvoie true, sinon false
-                        pokemon.visible = tableauThisPokemonType.some(type => {
-                            return typeSelected.includes(type)
-                        })
-                    // S'il y a plus de 1 filtre
-                    }else{
-                        // Pour ce pokemon, s'il a un type qui n'est pas includes dans le string de filtre, on renvoie false, sinon true
-                        pokemon.visible = tableauThisPokemonType.every(type => {
-                            return typeSelected.includes(type)
-                        })
-                    } 
-                }
-                pokemon.typeJoin = tableauThisPokemonType.join("")
-            })
-        }
-        // Si le nombre de type selectionner n'est pas superieur à 0
-        else{
-            // Tout les pokemons sont rendu visible
-            copyPokemonsList.forEach((pokemon) => {
-                // On prépare un tableau qui va contenir les types du pokemon en cours d'instance
-                const tableauThisPokemonType = []
-                pokemon.apiTypes.forEach(thisPokemonType => {
-                    // On rempli le tableau
-                    tableauThisPokemonType.push(thisPokemonType.name)
+
+        /////////////////////////////////////////////////
+        /////// Recupération des generation selectionner
+        /////////////////////////////////////////////////
+        const generationSelected = boutons.filter((bouton) => bouton.selected == true)
+
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////
+        /////// Modification pour chaque pokemon de la clef generationSelected en fonction des générations choisi
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        // Si au moin une generation est cliquer
+        if(generationSelected.length > 0){
+            // On supprime le logo
+            setLogoVisible(false)
+
+            // On réinitialise tout les generationSelected à false
+            copyPokemonsList.map(pokemon => pokemon.generationSelected = false)
+
+            // On change la clef generationSelected des pokemons qui sont des generations selectionner
+            generationSelected.forEach(generation => {
+                copyPokemonsList.filter(pokemon => {
+                    if(pokemon.apiGeneration === generation.generation)return true
+                }).map(pokemon => {
+                    pokemon.generationSelected = true
                 })
-                // On attribut à ce pokemon la clef typeJoin et on modifie sa clef visible
-                pokemon.typeJoin = tableauThisPokemonType.join("")
-                pokemon.visible = true
             })
-        }
+
+
+            ///////////////////////////////////////////////////////////
+            // Changement de la clef visible selon le type du pokemon
+            ///////////////////////////////////////////////////////////
+
+            // On créé un tableau qui correspond à tout les types selectionner
+            const tableauTypeSelected = boutonsSelectType.filter((element) => element.selected === true)
+            // On prépare une string de filtre qui va servir à écrire tout les nom de type séléctionner
+            let typeSelected = ""
+            // On rempli le string de filtre
+            tableauTypeSelected.forEach((type) => typeSelected += type.name)
+
+
+
+            // Si le nombre de type selectionner est supérieur à 0
+            if(tableauTypeSelected.length != 0){
+                // Pour chaque objet pokemon de la liste
+                copyPokemonsList.forEach(pokemon => {
+                    // Si la génération du pokemon est Selected
+                    if(pokemon.generationSelected === true){
+                        // On prépare un tableau qui va contenir les types du pokemon en cours d'instance
+                        const tableauThisPokemonType = []
+                        // Si le pokemon en cours d'instance a moin de type que le nombre de filtre en cours, il est rendu invisible, il ne rentre pas dans les critères de selection
+                        if(pokemon.apiTypes.length < tableauTypeSelected.length){pokemon.visible = false}
+                        // Si le pokemon en cours d'instance a autant ou plus de type que le nombre de filtre en cours
+                        else{
+                            // Pour chaque types de ce pokemon
+                            pokemon.apiTypes.forEach(thisPokemonType => {
+                                // On rempli le tableau
+                                tableauThisPokemonType.push(thisPokemonType.name)
+                            })
+                            // S'il n'y a qu'un seul filtre
+                            if(tableauTypeSelected.length === 1){
+                                // pour ce pokemon,on verifie si au moin l'un de ces types est includes dans le string de filtre, si oui, renvoie true, sinon false
+                                pokemon.visible = tableauThisPokemonType.some(type => {
+                                    return typeSelected.includes(type)
+                                })
+                            // S'il y a plus de 1 filtre
+                            }else{
+                                // Pour ce pokemon, s'il a un type qui n'est pas includes dans le string de filtre, on renvoie false, sinon true
+                                pokemon.visible = tableauThisPokemonType.every(type => {
+                                    return typeSelected.includes(type)
+                                })
+                            } 
+                        }
+                        pokemon.typeJoin = tableauThisPokemonType.join("")
+                    }
+                    // Si la génération du pokemon n'est pas selected
+                    else{
+                        pokemon.visible = false
+                    }
+                })
+            }
+            // Si le nombre de type selectionner n'est pas superieur à 0
+            else{
+                // Tout les pokemons sont rendu visible si leurs générations est selected
+                copyPokemonsList.forEach((pokemon) => {
+                    // Si leurs génération est selected
+                    if(pokemon.generationSelected === true){
+                        // On prépare un tableau qui va contenir les types du pokemon en cours d'instance
+                        const tableauThisPokemonType = []
+                        pokemon.apiTypes.forEach(thisPokemonType => {
+                            // On rempli le tableau
+                            tableauThisPokemonType.push(thisPokemonType.name)
+                        })
+                        // On attribut à ce pokemon la clef typeJoin et on modifie sa clef visible
+                        pokemon.typeJoin = tableauThisPokemonType.join("")
+                        pokemon.visible = true
+                    }
+                    // Si leurs générations n'est pas selected
+                    else{
+                        pokemon.visible = false
+                    }
+                })
+            }
         // On setState la liste de pokemon
         setPokemonsList(copyPokemonsList)
-    }
+        }else{
+            // On ajoute le logo
+            setLogoVisible(true)
+        }}
+        
 
 
     return(
-        <StateContext.Provider value={{pokemonsList, setPokemonsList, logoVisible, setLogoVisible, boutonsSelectType, setBoutonsSelectType, filtrage, profilPokemon, setProfilPokemon}}>
+        <StateContext.Provider value={{pokemonsList, setPokemonsList, logoVisible, setLogoVisible, boutonsSelectType, setBoutonsSelectType, filtrage, profilPokemon, setProfilPokemon, boutons, setBoutons}}>
             {children}
         </StateContext.Provider>
     )
